2c17c7943af3e8c368b7615f90996cb6fde90616
boris <borislav2003@gmail.com>
2022-12-26 14:32:34 +0500

    3.3.2

--- src/utils/salaryparser/salaryparser.py
+++ src/utils/salaryparser/salaryparser.py
@@ -3,22 +3,22 @@ import pandas as pd
 
 class SalaryParser:
 	def __init__(self, file_name: str) -> None:
-		self.__currencies = pd.read_csv('../currencyparser/data.csv')
+		self.__currencies = pd.read_csv('../currency.csv')
 		self.__available = list(self.__currencies.keys()[2:])
 		self.__filename = file_name
-	
-	def parse_salary(self) -> None:
+		
+	def process_salaries(self) -> None:
 		"""
-		Срздаёт обработанный CSV файл
+		Форматирует CSV файл
 		:return: None
 		"""
 		salaries = []
-		to_remove = []
+		remove = []
 		dataframe = pd.read_csv(self.__filename)
 		
-		for row in dataframe.itertuples():
-			s_from = str(row[2])
-			s_to = str(row[3])
+		for vacancy in dataframe.itertuples():
+			s_from = str(vacancy[2])
+			s_to = str(vacancy[3])
 			
 			if s_from != 'nan' and s_to != 'nan':
 				salary = float(s_from) + float(s_to)
@@ -27,24 +27,24 @@ class SalaryParser:
 			elif s_from == 'nan' and s_to != 'nan':
 				salary = float(s_to)
 			else:
-				to_remove.append(int(row[0]))
+				remove.append(int(vacancy[0]))
 				continue
 				
-			if row[4] == 'nan' or row[4] not in self.__available:
-				to_remove.append(int(row[0]))
+			if vacancy[4] == 'nan' or vacancy[4] not in self.__available:
+				remove.append(int(vacancy[0]))
 				continue
 				
-			if row[4] != 'RUR':
-				date = row[6][:7]
-				coefficient = self.__currencies[self.__currencies['date'] == date][row[4]].iat[0]
+			if vacancy[4] != 'RUR':
+				date = vacancy[6][:7]
+				coefficient = self.__currencies[self.__currencies['date'] == date][vacancy[4]].iat[0]
 				salary *= coefficient
 				
 			salaries.append(salary)
 			
-		dataframe.drop(labels=to_remove, axis=0, inplace=True)
+		dataframe.drop(labels=remove, axis=0, inplace=True)
 		dataframe.drop(labels=['salary_to', 'salary_from', 'salary_currency'], axis=1, inplace=True)
 		dataframe['salary'] = salaries
-		dataframe.to_csv('res.csv')
+		dataframe.head(100).to_csv('100.csv')
 
 
-SalaryParser('../../../vacancies_dif_currencies.csv').parse_salary()
+SalaryParser('../vacancies_dif_currencies.csv').process_salaries()
